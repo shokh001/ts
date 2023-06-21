3ebb1e7fc9807bd6fd8e8238dfe7c2bcf26fe7e6
shokh001 <shohjahondilmurodov56@gmail.com>
2023-06-21 20:45:31 +0500

    basic data types

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+let a = 12;
+let b = 'text';
+let c = false;
+let d = null;
+let e = undefined;
+let f = { name: 'Shoh' };
+f.name = 'Shohjahon';
+let g;
+g = 12;
+g = 'text';
+g = false;
+g = { name: 'Dilmurod' };
+g = function () { };
+g = [];
+g = null;
+g = undefined;
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,20 @@
+let a: number = 12;
+let b: string = 'text';
+let c: boolean = false;
+let d: null = null;
+let e: undefined = undefined;
+
+let f: { name: string } = { name: 'Shoh' };
+f.name = 'Shohjahon';
+
+
+let g: any;
+
+g = 12;
+g = 'text';
+g = false;
+g = { name: 'Dilmurod' };
+g = function () { };
+g = [];
+g = null;
+g = undefined;
