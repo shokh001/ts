8ddebe9a98be65185fafab4ef819cb558f8dd2fa
shokh001 <shohjahondilmurodov56@gmail.com>
2023-06-22 12:01:07 +0500

    Tiplarni o'zgartirish va birlashtirish

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 "use strict";
-//TS => functions, signature functions, function overloads
-// function decloaration
-function pow(x, y) {
-    return `${x} ** ${y} = ${x ** y}`;
+//TS => Tiplarni o'zgartirish va birlashtirish
+// havfli usul
+let a = 10;
+let b = a;
+// yaxshi usul
+// unknown any ga ekvavalent
+let c = 20.224234;
+// let d: number = c; error
+let e = c; // --> type change first way
+let f = c; // --> type change second way
+// (c as number).toFixed(2) mehtodlarni ishlatish
+// (<number>c).toString() mehtodlarni ishlatish
+// union types --> tiplar birlashmasi
+let h = 10;
+h = 'str';
+// h = true error
+// literal types --> faqat belgilangan typelarni qabul qiladi
+let s;
+s = 'sm';
+let s2 = "MD";
+s2 = "LG";
+s2 = "SM";
+let s3 = false;
+s3 = "MD";
+let obj;
+// obj = {}  error
+obj = { name: 'Shoh' };
+obj = { age: 22 };
+obj = { name: 'Shohjahon', age: 22 };
+let obj2;
+obj2 = { name: 'Dilmurod', age: 22 };
+let obj3 = { name: 'Shoh' };
+obj3 = { name: 'Shoh', age: 22 };
+// obj3 = { age: 22 }; error
+if ('age' in obj3) { // in operatori
+    console.log('Have');
 }
-// arrow function
-const add = (x, y) => x + y;
-function voidFunction(x) {
-    console.log(x);
+else {
+    console.log('Don`t have');
 }
-// hech qachon tugamaydigan siklli joylarada ishlatiladi
-// tugallanmaydigan function
-function neverFunction(s) {
-    throw new Error(s);
-}
-///signature functions
-let c;
-c = function (a, b) {
-    return `${b}: ${a}`;
-};
-function overloadsFunc(x, y) {
-    if (typeof x === "number" && typeof y === "number") {
-        return x + y;
-    }
-    else {
-        return `${x} ${y}`;
-    }
-}
-console.log(overloadsFunc(1, 3));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,47 +1,65 @@
-//TS => functions, signature functions, function overloads
+//TS => Tiplarni o'zgartirish va birlashtirish
 
-// function decloaration
-function pow(x: number, y: number): string {
-    return `${x} ** ${y} = ${x ** y}`;
+// havfli usul
+let a: any = 10;
+let b: number = a;
+
+// yaxshi usul
+// unknown any ga ekvavalent
+let c: unknown = 20.224234;
+// let d: number = c; error
+
+let e: number = <number>c; // --> type change first way
+let f: number = c as number; // --> type change second way
+// (c as number).toFixed(2) mehtodlarni ishlatish
+// (<number>c).toString() mehtodlarni ishlatish
+
+// union types --> tiplar birlashmasi
+let h: number | string = 10;
+h = 'str';
+// h = true error
+
+// literal types --> faqat belgilangan typelarni qabul qiladi
+let s: "sm" | "md" | "lg" | boolean;
+s = 'sm';
+// s ='a' error
+// s=10 error
+
+
+// type aliases --> tipga nom berib typelarni yig`ib olish va type sifatida ishlatish
+
+type Sizes = "SM" | "MD" | "LG" | boolean;
+let s2: Sizes = "MD";
+s2 = "LG";
+s2 = "SM";
+
+let s3: Sizes = false;
+s3 = "MD";
+
+type OBJ = { name: string } | { age: number };
+let obj: OBJ;
+
+// obj = {}  error
+obj = { name: 'Shoh' };
+obj = { age: 22 };
+obj = { name: 'Shohjahon', age: 22 };
+
+
+type OBJand = { name: string } & { age: number };
+let obj2: OBJand;
+
+obj2 = { name: 'Dilmurod', age: 22 }
+// obj2 = { name: 'Dilmurod'} error
+// obj2 = { age: 22} error
+
+type OBJ3 = { name: string, age?: number }; // option maydon ? orqali kiritiladi
+let obj3: OBJ3 = { name: 'Shoh' };
+obj3 = { name: 'Shoh', age: 22 };
+// obj3 = { age: 22 }; error
+
+
+if ('age' in obj3) { // in operatori
+    console.log('Have');
+} else {
+    console.log('Don`t have');
 }
-
-// arrow function
-const add = (x: number, y: number): number => x + y;
-
-function voidFunction(x: number): void {
-    console.log(x);
-}
-
-// hech qachon tugamaydigan siklli joylarada ishlatiladi
-// tugallanmaydigan function
-function neverFunction(s: string): never {
-    throw new Error(s)
-}
-
-
-
-///signature functions
-
-let c: (x: number, y: string) => string
-
-c = function (a: number, b: string): string {
-    return `${b}: ${a}`
-}
-
-// console.log(c(3, 'k'));
-
-
-/// function overloads --> funksiyalarga qo'shimcha tiplar berish imkoniyatini yaratadi
-
-function overloadsFunc(x: number, y: number): number;
-function overloadsFunc(x: string, y: number): string;
-
-function overloadsFunc(x: any, y: any): any {
-    if (typeof x === "number" && typeof y === "number") {
-        return x + y;
-    } else {
-        return `${x} ${y}`
-    }
-}
-
-console.log(overloadsFunc(1, 3));
