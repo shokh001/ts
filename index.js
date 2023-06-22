172adc2f884dfc79d5136aab0e30694783218555
shokh001 <shohjahondilmurodov56@gmail.com>
2023-06-22 17:38:37 +0500

    classes & inheritance

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
 "use strict";
-//TS => classes
+//TS => classes & inheritance
 class Person {
     constructor(name, age) {
+        this._name = "";
+        this._age = 0;
         this._name = name;
         this._age = age;
     }
-    info(value) {
-        return `${this._name} - ${this._age + value} da`;
-    }
-    static description() {
-        return 'Bu klass Person haqida';
-    }
-    get name() {
-        return this._name;
-    }
-    set name(value) {
-        this._name = value;
-    }
-    get age() {
-        return this._age;
-    }
-    set age(value) {
-        if (value > 0 && value < 100) {
-            this._age = value;
-        }
-        else {
-            throw new Error('Notugri yosh');
-        }
+    sayHello() {
+        return `Assalomu alaykum ${this._name}`;
     }
 }
-Person.isFly = false; // static kalit suzi bilan e`lon qilingan maydon classni uziga tegishli buladi undon olingan obyetga emas
-const shoh = new Person('shoh', 22);
-const dilmurodov = new Person('Dilmurodov', 22);
-// console.log(shoh);
-// console.log(dilmurodov);
-// console.log(shoh.info(2));
-// console.log(Person.description());
-shoh.age = 24;
-// console.log(shoh);
-if (shoh instanceof Person) { // instanceof object classga tegishli ekanliligini tekshiradi
-    console.log(true);
+class Student extends Person {
+    constructor(name, age, group, course) {
+        super(name, age);
+        this._group = "";
+        this._course = 0;
+        this._group = group;
+        this._course = course;
+    }
+    sayHello() {
+        const parentFunc = super.sayHello(); // otasidagi methoddan meros olish
+        return `${parentFunc}. Men ${this._course} kursda o'qiyman`;
+    }
 }
-else {
-    console.log(false);
+class Teacher extends Person {
+    constructor(name, age, disciplines) {
+        super(name, age);
+        this.disciplines = [];
+        this.disciplines = disciplines;
+    }
+    info() {
+        const parent = super.sayHello();
+        return `${parent}. Men o'qituvchiman`;
+    }
 }
+const shoh = new Person('Shohjahon', 22);
+console.log(shoh);
+console.log(shoh.sayHello());
+const dilmurod = new Student('Dilmurod', 23, '213-19', 4);
+console.log(dilmurod);
+console.log(dilmurod.sayHello());
+const newDilmurod = dilmurod;
+// newDilmurod.
+console.log(newDilmurod);
+console.log(newDilmurod.sayHello());
+const abror = new Teacher("Abror", 32, ['React, JS, TS']);
+console.log(abror);
+console.log(abror.info());
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,59 +1,63 @@
-//TS => classes
+//TS => classes & inheritance
 
 class Person {
-    _name: string;
-    _age: number
+    _name: string = "";
+    _age: number = 0;
 
     constructor(name: string, age: number) {
         this._name = name;
         this._age = age
     }
 
-    info(value: number): string {
-        return `${this._name} - ${this._age + value} da`
-    }
-
-    static isFly = false; // static kalit suzi bilan e`lon qilingan maydon classni uziga tegishli buladi undon olingan obyetga emas
-    static description(): string {
-        return 'Bu klass Person haqida'
-    }
-
-    get name(): string {
-        return this._name
-    }
-
-    set name(value: string) {
-        this._name = value
-    }
-
-    get age(): number {
-        return this._age
-    }
-
-    set age(value: number) {
-        if (value > 0 && value < 100) {
-            this._age = value
-        } else {
-            throw new Error('Notugri yosh')
-        }
+    sayHello(): string {
+        return `Assalomu alaykum ${this._name}`
     }
 }
 
-const shoh: Person = new Person('shoh', 22);
-const dilmurodov: Person = new Person('Dilmurodov', 22)
+class Student extends Person {
+    _group: string = "";
+    _course: number = 0;
 
-// console.log(shoh);
-// console.log(dilmurodov);
-// console.log(shoh.info(2));
-// console.log(Person.description());
-shoh.age = 24;
-// console.log(shoh);
+    constructor(name: string, age: number, group: string, course: number) {
+        super(name, age)
+        this._group = group;
+        this._course = course;
+    }
 
-if(shoh instanceof Person) { // instanceof object classga tegishli ekanliligini tekshiradi
-    console.log(true);    
-} else {
-    console.log(false);
-    
+    sayHello(): string {
+        const parentFunc = super.sayHello() // otasidagi methoddan meros olish
+        return `${parentFunc}. Men ${this._course} kursda o'qiyman`;
+    }
 }
 
+class Teacher extends Person {
+    disciplines: string[] = [];
 
+    constructor(name: string, age: number, disciplines: string[]) {
+        super(name, age);
+        this.disciplines = disciplines
+    }
+
+    info():string {
+        const parent = super.sayHello()
+        return `${parent}. Men o'qituvchiman`
+    }
+}
+
+const shoh: Person = new Person('Shohjahon', 22);
+console.log(shoh);
+console.log(shoh.sayHello());
+
+const dilmurod: Student = new Student('Dilmurod', 23, '213-19', 4)
+console.log(dilmurod);
+console.log(dilmurod.sayHello());
+
+
+const newDilmurod: Person = <Person>dilmurod;
+// newDilmurod.
+console.log(newDilmurod);
+console.log(newDilmurod.sayHello());
+
+const abror: Teacher = new Teacher("Abror", 32, ['React, JS, TS'])
+console.log(abror);
+console.log(abror.info());
